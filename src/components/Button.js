818a55013eb0e34186b91bce365116f71da410f2
keshav-potomac <ksingh@potomactpl.com>
2019-10-07 18:24:08 +0530

    code merged.

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+export const ButtonContainer = styled.button`
+ text-transform:capitalize;
+ font-size:1.4rem;
+ background: transparent;
+ border:0.05rem solid var(--lightBlue);
+ color:var(--lightBlue);
+ border-radius: 0.5rem;
+ padding: 0.2rem 0.5rem;
+ cursor:pointer;
+ margine: 0.2rem 0.5rem 0.rem 0;
+ transition:all 0.5s eae-in-out;
+ &:hover{
+     background:var(--lightBLue);
+     color:var(--mainBlue);
+ }
+ &:focus{
+     outline:none;
+ }
+`;
+
+
+export const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    tex-transform: capitalize!important;
+}
+`;
---
+++ src/context.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { storeProducts , detailProduct } from './data';
+const ProductContext =  React.createContext();
+ class ProductProvider extends Component {
+     state = {
+         products: [],
+         detailProduct: detailProduct
+     }
+
+     componentDidMount(){
+        this.setProducts();
+    }
+     setProducts = () =>{
+         let tempProducts = [];
+         storeProducts.forEach(item => {
+             const singleItem = {...item};
+             tempProducts = [...tempProducts, singleItem];
+         })
+         this.setState(()=>{
+             return {products: tempProducts}
+         });
+     }
+    
+     handleDetail = () => {
+         console.error("Hello ffrom drails")
+     }
+     
+     addToCart = (id) => {
+        console.error("Hello from add to cart id is "+id)
+    }
+  
+    render() {
+        return (
+            <ProductContext.Provider value={{
+                ...this.state, handleDetail: this.handleDetail,addToCart:this.addToCart
+            }}>
+              
+                {this.props.children}
+            </ProductContext.Provider>
+        )
+    }
+}
+
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider , ProductConsumer}
